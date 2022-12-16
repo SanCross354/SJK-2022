562d686e527fc7d0c70fee5507e89b6831e676f9
yoangga <21523235@students.uii.ac.id>
2022-12-16 21:11:36 +0700

    baru

--- public/html/RESET GAGAL.php
+++ public/html/RESET GAGAL.php
@@ -11,7 +11,7 @@ echo '</script>';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN PAGE</title>
-    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href ="/public/css/style.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
